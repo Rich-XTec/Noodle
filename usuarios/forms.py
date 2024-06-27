db0528a3e55dc889af9b87f92dd05f3d1514166b
Rich-XTec <143571124+Rich-XTec@users.noreply.github.com>
2024-06-27 19:37:28 -0300

    Update forms.py

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -7,14 +7,14 @@ class ModelUsuarioCreateForm(UserCreationForm):
         fields = ('first_name', 'last_name', 'fone')
         labels = {'username': 'Username/E-mail'}
 
-        def save(self, commit=True):
-            user = super().save(commit=False) #salvar a variavel, mas ainda não no banco
-            user.set_password(self.cleaned_data["password1"])
-            user.email = self.cleaned_data["username"]
+    def save(self, commit=True):
+        user = super().save(commit=False) #salvar a variavel, mas ainda não no banco
+        user.set_password(self.cleaned_data["password1"])
+        user.email = self.cleaned_data["username"]
 
-            if commit:
-                user.save()
-            return user
+        if commit:
+            user.save()
+        return user
 
 class ModelUsuarioChangeForm(UserChangeForm):
     class Meta:
