032f54117cc2ff9a0457683431550757cd7927eb
Rich-XTec <143571124+Rich-XTec@users.noreply.github.com>
2024-06-28 15:51:28 -0300

    Update views.py

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-from .models import ModelUsuario
-from .forms import ContatoForm
+# from .models import ModelUsuario
+# from .forms import ContatoForm
 
-class IndexView(FormView):
-    template_name = 'index.html'
-    form_class = ContatoForm
-    success_url = reverse_lazy('index')
+# class IndexView(FormView):
+#     template_name = 'index.html'
+#     form_class = ContatoForm
+#     success_url = reverse_lazy('index')
 
-    def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
-        context['servicos'] = Servico.objects.all()
-        #context['servicos'] = Servico.objects.order_by('?').all()
-        context['funcionarios'] = Funcionario.objects.all()
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super(IndexView, self).get_context_data(**kwargs)
+#         context['servicos'] = Servico.objects.all()
+#         #context['servicos'] = Servico.objects.order_by('?').all()
+#         context['funcionarios'] = Funcionario.objects.all()
+#         return context
